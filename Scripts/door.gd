6437c56f572a89818ea03fe6c4fd370e4ff1eac7
AnastasiyaDmytryk <90424704+AnastasiyaDmytryk@users.noreply.github.com>
2024-10-04 17:08:02 -0400

    Added the power up

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,21 +1,24 @@
 extends Area2D
-func getRandomRoom():
-	var i = randi()%globalRooms.roomSet.size()
-	return globalRooms.roomSet[i]
 
-# Called when the node enters the scene tree for the first time.
+var scene_changing = false  # Add a flag to prevent repeated scene changes
+
+func getRandomRoom():
+	var i = randi() % GlobalRooms.roomSet.size()
+	return GlobalRooms.roomSet[i]
+
+#Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
 
 func _on_body_entered(body):
-
-	call_deferred("change_scene_to_random_room")
-
+	# Only change the scene if it's not already changing
+	if not scene_changing:
+		scene_changing = true
+		call_deferred("change_scene_to_random_room")
 func change_scene_to_random_room():
-	get_tree().change_scene_to_file(getRandomRoom())
+	GlobalRooms.currentScene= getRandomRoom()
+	get_tree().change_scene_to_file(GlobalRooms.currentScene)

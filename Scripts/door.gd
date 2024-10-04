0b4118713107128c2752dd6203375691211ef021
tdwilson0501 <aw2933@gmail.com>
2024-10-04 15:00:15 -0400

    Fixed door.gd

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -1,21 +1,24 @@
 extends Area2D
+
+var scene_changing = false  # Add a flag to prevent repeated scene changes
+
 func getRandomRoom():
-	var i = randi()%GlobalRooms.roomSet.size()
+	var i = randi() % GlobalRooms.roomSet.size()
 	return GlobalRooms.roomSet[i]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 	
-
 func _on_body_entered(body):
-
-	call_deferred("change_scene_to_random_room")
+	# Only change the scene if it's not already changing
+	if not scene_changing:
+		scene_changing = true
+		call_deferred("change_scene_to_random_room")
 
 func change_scene_to_random_room():
 	get_tree().change_scene_to_file(getRandomRoom())
